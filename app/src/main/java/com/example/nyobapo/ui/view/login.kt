7ddf6c4368fc132ea7855e7d381c3e2e55d82250
Dave Sachio Usanto <dsachiousanto@student.ciputra.ac.id>
2025-10-22 09:03:26 +0700

    commit

---
+++ app/src/main/java/com/example/nyobapo/ui/view/login.kt
@@ -0,0 +1,18 @@
+package com.example.nyobapo.ui.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun view(){
+    Text("babi")
+    // Implementation of the Text composable
+}
+
+@Preview(showBackground = true)
+@Composable
+fun viewPreview() {
+    view()
+}
